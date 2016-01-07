5c984a94fc92ceebc00aa5dfe472dc0577e82f13
Jordan Poles <jpoles1@gmail.com>
2016-01-07 10:18:32 -0500

    Added population data. Looking polished.

--- clean.py
+++ clean.py
@@ -11,6 +11,10 @@ def abrevtost(x):
 rawdata["StateAbrev"] = rawdata["State"]
 rawdata["State"] = rawdata["State"].apply(abrevtost)
 statesum = rawdata.groupby("State").sum().iloc[:,1:]
+statepop = pd.read_csv("statepop.csv").loc[:,["NAME", "POPESTIMATE2014"]]
+statepop.columns = ["State", "Population"]
+statesum = statesum.merge(statepop, left_index=1, right_on="State", how="left")
+statesum.index = statesum["State"]
 maxes = statesum.apply(max, axis=0)
 mins = statesum.apply(min, axis=0)
 outputdat = {"vals": statesum.to_dict(), "maxes": maxes.to_dict(), "mins": mins.to_dict()}
